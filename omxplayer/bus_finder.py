76d583294fd90012f17d6352bb1c7b6dae6c7444
Will Price <will.price94@gmail.com>
2014-09-25 17:17:51 +0100

    Wait for DBus file to become available at startup

--- omxplayer/bus_finder.py
+++ omxplayer/bus_finder.py
@@ -1,3 +1,5 @@
+import os.path
+import time
 from logging import getLogger
 
 logger = getLogger(__name__)
@@ -9,9 +11,14 @@ class BusFinder(object):
         logger.debug('BusFinder initialised with path: %s' % path)
 
     def get_address(self):
+        self.wait_for_file()
         logger.debug('Opening file at %s' % self.path)
         with open(self.path, 'r') as f:
             logger.debug('Opened file at %s' % self.path)
             self.address = f.read().strip()
             logger.debug('Address \'%s\' parsed from file' % self.path)
         return self.address
+
+    def wait_for_file(self):
+        while not os.path.isfile(self.path):
+            time.sleep(0.05)
