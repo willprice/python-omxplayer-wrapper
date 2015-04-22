31c603cab9b40d11ce25d93f8ba752f3e22864d8
Will Price <will.price94+dev@gmail.com>
2015-04-22 16:27:50 +0000

    Remove DBus dependency in setup.py since its not on PyPi

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     keywords='omxplayer pi raspberry raspberrypi raspberry_pi library',
 
     packages=find_packages(exclude=['*tests']),
-    install_requires=['dbus'],
+    install_requires=[],
     extras_require={
         'test': ['nosetests']
     }
