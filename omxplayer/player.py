1f530c7598d85d345850ff266d63ae89f32e3375
Stewart Watkiss <stewart@penguintutor.com>
2016-09-30 11:09:45 +0100

    Minor change to description for is_playing() to reflect correct return type

--- omxplayer/player.py
+++ omxplayer/player.py
@@ -382,7 +382,7 @@ class OMXPlayer(object):
     def is_playing(self):
         """
         Returns:
-            None:
+            bool: Whether the player is playing
         """
         self._is_playing = (self.playback_status() == "Playing")
         logger.info("Playing?: %s" % self._is_playing)
