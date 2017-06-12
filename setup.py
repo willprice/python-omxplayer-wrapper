860dcaeb3cfd3d35dfde6306c3ebd915d24db37a
Will Price <will.price94@gmail.com>
2017-06-12 22:28:08 +0100

    [version] Bump version from 0.2.2 to 0.2.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='will.price94+dev@gmail.com',
     url='https://github.com/willprice/python-omxplayer-wrapper',
 
-    version='0.2.1',
+    version='0.2.3',
 
     description='Control OMXPlayer on the Raspberry Pi',
     long_description='Control OMXPlayer on the Raspberry Pi through DBus',
